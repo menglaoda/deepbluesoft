7b1e1d60ac1605a0a8f431de2e6cef6ba3bdd72a
menglaoda <1415940875@qq.com>
2020-08-12 17:40:30 +0800

    2

--- js/public.js
+++ js/public.js
@@ -3,4 +3,10 @@ $(function(){
     $(".myheader").load("header.html");
     //加载尾部
     $(".myfooter").load("footer.html");
+    //首页启动动画
+    setTimeout(function(){
+    	$("body").css("overflow","visible");
+    	$(".content").css("display","block");
+    },3000)
+    
 })
